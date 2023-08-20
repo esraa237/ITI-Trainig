9488cf70d5b0cff10b75b1a664759929b4a0da95
esraa237 <esraafoda2000@gmail.com>
2023-08-20 13:10:43 +0300

    finish Day1 week4

---
+++ week4-ASP.net/Day1/firstProject/firstProject/Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace firstProject.Controllers
+{
+    public class FirstController : Controller
+    {
+        //First/Show
+        public string Show()
+        {
+            return "MVC";
+        }
+        //First/Add
+        public int Add()
+        {
+            return 200;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
